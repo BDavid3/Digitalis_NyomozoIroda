fb6e87b4474de2943d18ac8148d8eedceb3fe3b8
Balázs Dávid <gubamagos@gmail.com>
2026-01-30 09:21:38 +0100

    Tanú osztály létrehozása, adattagok létrehozása

---
+++ Digitalis_NyomozoIroda/Tanú.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digitalis_NyomozoIroda
+{
+	internal class Tanú
+	{
+		private Tanú Person;
+		private string vallomas;
+		private int vallomasdatum;
+	}
+}
