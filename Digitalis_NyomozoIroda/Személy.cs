ec117a29aa276e7e1f0126f4f0f54fb6855c9716
Balázs Dávid <gubamagos@gmail.com>
2026-01-30 09:13:02 +0100

    Személy osztály létrehozása, az adattagokkal együtt

---
+++ Digitalis_NyomozoIroda/Személy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digitalis_NyomozoIroda
+{
+	internal class Személy
+	{
+		private string nev;
+		private int eletkor;
+		private string megjegyzes;
+	}
+}
