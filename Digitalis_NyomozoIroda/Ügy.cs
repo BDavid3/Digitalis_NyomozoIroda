76e305562ff819653a54d2e74eab1539089a62b1
Balázs Dávid <gubamagos@gmail.com>
2026-01-30 09:09:46 +0100

    Ügy osztály létrehozása, + adattagoké is

---
+++ Digitalis_NyomozoIroda/Ügy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digitalis_NyomozoIroda
+{
+	internal class Ügy
+	{
+		private string ugyazonosito;
+		private string cim;
+		private string leiras;
+		private string allapot;
+		private List<Ügy> szemelyek;
+		private List<Ügy> bizonyitekok;
+	}
+}
