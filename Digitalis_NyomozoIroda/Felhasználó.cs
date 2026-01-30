0dcc4696171b05e665db0b8dff9ef2c51f78e334
Balázs Dávid <gubamagos@gmail.com>
2026-01-30 08:59:36 +0100

    Felhasználó osztály létrehozása az adattagokkal

---
+++ Digitalis_NyomozoIroda/Felhasználó.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digitalis_NyomozoIroda
+{
+	internal class Felhasználó
+	{
+		private string nev;
+		private string azonosito;
+		private string szerepkor;
+	}
+}
