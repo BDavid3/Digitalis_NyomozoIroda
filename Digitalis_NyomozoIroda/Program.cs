d422307e04896333ee50b9c28c678916961aef79
David <gubamagos@gmail.com>
2026-02-06 09:23:00 +0100

    tanú class kiirattatása

--- Digitalis_NyomozoIroda/Program.cs
+++ Digitalis_NyomozoIroda/Program.cs
@@ -6,6 +6,7 @@
         {
 			Személy sz1 = new Személy("Bekre Pál",28,"Fekete rövid haj, kb: 190cm, fehér bőrű férfi");
 			Személy sz2 = new Személy("Lakatos Márió", 20, "kopasz, kb: 175cm, barna bőrű férfi");
+			Személy sz3 = new Személy("Kiss András", 29, "szőke haj, kb: 190cm, fehér bőrű férfi");
 			Bizonyíték b1 = new Bizonyíték("FEGYVER1","kamerafelvétel","maroklőfegyver, valószínűleg GLOCK-19-es típusú",4);
 			Bizonyíték b2 = new Bizonyíték("Újlenyomat1","fotó","nagymennyiségű újlenyomat található a bolton belül",5);
 			Felhasználó nyomozó = new Felhasználó("Bendős Dávid","IEA38234","Nyomozó");
@@ -16,7 +17,9 @@
 			boltibünügy.bizonyitekhozzaadas(b2);
 			Console.WriteLine(boltibünügy);
 			Gyanusított gy1 = new Gyanusított(sz2,90,"megfigyelt");
+			Tanú t1 = new Tanú(sz3, "A rablás során egy bokor mögött megbújva, szemtanúja voltam az egész büncselekménynek. A telefonommal rögzítettem az egészet",2026,02,14);
 			Console.WriteLine(gy1);
+			Console.WriteLine(t1);
 		}
     }
 }
--- Digitalis_NyomozoIroda/Tanú.cs
+++ Digitalis_NyomozoIroda/Tanú.cs
@@ -10,21 +10,27 @@ namespace Digitalis_NyomozoIroda
 	{
 		private Személy Person;
 		private string vallomas;
-		private DateOnly vallomasdatum;
+		private int year;
+		private int month;
+		private int day;
 
-		public Tanú(Személy person, string vallomas, DateOnly vallomasdatum)
+		public Tanú(Személy person, string vallomas, int year, int month, int day)
 		{
 			Person = person;
 			this.vallomas = vallomas;
-			this.vallomasdatum = vallomasdatum;
+			this.year = year;
+			this.month = month;
+			this.day = day;
 		}
-
 		public string Vallomas { get => vallomas; set => vallomas = value; }
-		public DateOnly Vallomasdatum { get => vallomasdatum; set => vallomasdatum = value; }
+		public int Year { get => year; set => year = value; }
+		public int Month { get => month; set => month = value; }
+		public int Day { get => day; set => day = value; }
 		internal Személy Person1 { get => Person; set => Person = value; }
+
 		public override string ToString()
 		{
-			return $"-------Tanú:-------\n\tSzemély: {this.Person}\n\tVallomás szövege: {this.vallomas}\n\t Vallomás dátuma: {this.vallomasdatum}";
+			return $"--------------Tanú:--------------\n\t{this.Person}\n\tVallomás szövege: {this.vallomas}\n\t Vallomás dátuma:{this.year}/{this.month}/{this.day}";
 		}
 	}
 }
